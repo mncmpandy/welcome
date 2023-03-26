c82f3a5457cc4a9bf9c5d40ae38f51c03d4b41ed
mncmpandy <mncmpandy@gmail.com>
2023-03-26 21:30:06 +0200

    Update submit.php

--- submit.php
+++ submit.php
@@ -31,17 +31,17 @@ if (isset($_POST['Email'])) {
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
 
     if (!preg_match($email_exp, $email)) {
-        $error_message .= 'The Email address you entered does not appear to be valid.<br>';
+        $error_message = 'The Email address you entered does not appear to be valid.<br>';
     }
 
     $string_exp = "/^[A-Za-z .'-]+$/";
 
     if (!preg_match($string_exp, $name)) {
-        $error_message .= 'The Name you entered does not appear to be valid.<br>';
+        $error_message = 'The Name you entered does not appear to be valid.<br>';
     }
 
     if (strlen($message) < 2) {
-        $error_message .= 'The Message you entered do not appear to be valid.<br>';
+        $error_message = 'The Message you entered do not appear to be valid.<br>';
     }
 
     if (strlen($error_message) > 0) {
@@ -56,9 +56,9 @@ if (isset($_POST['Email'])) {
         return str_replace($bad, "", $string);
     }
 
-    $email_message .= "name: " . clean_string($name) . "\n";
-    $email_message .= "email: " . clean_string($email) . "\n";
-    $email_message .= "message: " . clean_string($message) . "\n";
+    $email_message = "name: " . clean_string($name) . "\n";
+    $email_message = "email: " . clean_string($email) . "\n";
+    $email_message = "message: " . clean_string($message) . "\n";
 
     // create email headers
     $headers = 'From: ' . $email . "\r\n" .
